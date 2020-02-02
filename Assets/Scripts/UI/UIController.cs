eb8dd108f2b954a7007cde33934102ce5a9b3a16
zippyl <ctacoh15l@gmail.com>
2020-02-02 13:04:52 +0200

    add menu scene, upd

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+    public static UIController Instance { get; set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
