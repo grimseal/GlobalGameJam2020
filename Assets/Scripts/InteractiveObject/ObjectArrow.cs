7cc101d2328d49358f619052c3f69fa40db514f8
tyoden <denis.tyossa@gmail.com>
2020-02-02 11:13:29 +0200

    Implement child controller value indicator

---
+++ Assets/Scripts/InteractiveObject/ObjectArrow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace InteractiveObject
+{
+    public class ObjectArrow : MonoBehaviour
+    {
+        public float MinValueAngle;
+
+        public float MaxValueAngle;
+
+        private Transform arrowTransform;
+        
+        private float val;
+        private float NormalizedValue
+        {
+            get => val;
+            set
+            {
+                val = value;
+                var angle = Mathf.Lerp(MinValueAngle, MaxValueAngle, val);
+                arrowTransform.rotation = Quaternion.Euler(0, 0, angle);
+            }
+        }
+
+        private void Awake()
+        {
+            arrowTransform = transform;
+        }
+
+        public void SetValue(float value)
+        {
+            NormalizedValue = 1f / 100f * value;
+        }
+
+    }
+}
